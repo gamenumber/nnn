613c1140a0589cd6056e9d1ca83b0f236fbbcee4
gamenumber <kimhunbin0226@gmail.com>
2024-02-22 10:09:15 +0900

    Protact 추가 문제 해결 & 색깔 바꾸기

--- My project/Assets/01.Scripts/Player/Skill/Trigger.cs
+++ My project/Assets/01.Scripts/Player/Skill/Trigger.cs
@@ -6,7 +6,7 @@ public class Trigger : MonoBehaviour
 {
 	private void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "EnemyBullet")
+		if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "EnemyBullet" || other.gameObject.tag == "Thunder" || other.gameObject.tag == "Wind")
 		{
 			Destroy(other.gameObject);
 		}
